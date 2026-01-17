667d911f6e96b8890392b1cc8d1f72eda4937db1
rvzapataworks <ryanvincentzapataworks@gmail.com>
2026-01-17 12:34:24 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+}, { threshold: 0.15 });
+
+reveals.forEach(r => observer.observe(r));
